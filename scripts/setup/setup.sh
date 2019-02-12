7853b405ed05b2d3812a585058d69166ec304c50
Rouke Pouw <34579399+RoukePouw@users.noreply.github.com>
2019-02-12 14:36:01 +0100

    Implement setup script and diag script

---
+++ scripts/setup/setup.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+OLDPATH="$PATH"
+WHEREAMI="`pwd`"
+
+# $HYBRIXD/node/scripts/setup  => $HYBRIXD
+SCRIPTDIR="`dirname \"$0\"`"
+HYBRIXD="`cd \"$SCRIPTDIR/../../..\" && pwd`"
+
+echo " [i] setup node."
+sh "$HYBRIXD/node/scripts/npm/setup.sh"
+echo " [i] setup interface."
+sh "$HYBRIXD/interface/scripts/npm/setup.sh"
+
+if [ -d "$HYBRIXD/deterministic" ];then
+    echo "[i] setup determinstic."
+    sh "$HYBRIXD/deterministic/scripts/npm/setup.sh"
+fi
+
+if [ -d "$HYBRIXD/tui-wallet" ];then
+    echo "[i] setup tui-wallet."
+    sh "$HYBRIXD/tui-wallet/scripts/npm/setup.sh"
+fi
+
+if [ -d "$HYBRIXD/web-wallet" ];then
+    echo "[i] setup web-wallet."
+    sh "$HYBRIXD/web-wallet/scripts/npm/setup.sh"
+fi
+
+if [ -d "$HYBRIXD/cli-wallet" ];then
+    echo "[i] setup cli-wallet."
+    sh "$HYBRIXD/cli-wallet/scripts/npm/setup.sh"
+fi
+export PATH="$OLDPATH"
+cd "$WHEREAMI"
+exit $?
