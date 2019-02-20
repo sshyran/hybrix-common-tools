3210fd0350fa13bc06c90ca8e032622d87a62de2
Rouke Pouw <roukepouw@gmail.com>
2019-02-20 11:14:48 +0000

    Fix lint for linux

--- scripts/lint/lint.sh
+++ scripts/lint/lint.sh
@@ -2,27 +2,35 @@
 OLDPATH="$PATH"
 WHEREAMI="`pwd`"
 
-
 # $HYBRIXD/node/scripts/npm  => $HYBRIXD
 SCRIPTDIR="`dirname \"$0\"`"
 HYBRIXD="`cd \"$SCRIPTDIR/../../..\" && pwd`"
 
+export PATH="$HYBRIXD/node/node_binaries/bin:$PATH"
+
 # Move to provided directory
 if [ -z "$2" ]; then
     cd "$HYBRIXD/$1"
+elif [ -z "$1" ]; then
+    cd "$WHEREAMI"
 else
     cd "$1"
 fi
 
 IFS=$'\n'
-STAGED_FILES=$(git diff --stat --cached --name-only master | grep ".jsx\{0,1\}$" | grep "^[^node_;]")
-ESLINT="$(git rev-parse --show-toplevel)/../common/node_modules/.bin/eslint"
+LINTLIST="/tmp/hybrixlint.list"
+git diff --stat --cached --name-only master | grep ".jsx\{0,1\}$" | grep -v "node_" > $LINTLIST
+ESLINT="$HYBRIXD/common/node_modules/.bin/eslint"
+
+echo "$STAGED_FILES"
 
 echo "\nPre Push:\n"
 
-if [ "$STAGED_FILES" = "" ]; then
-  echo "\033[42mNO CHANGES\033[0m\n"
-  exit 0
+if [ -s "$STAGED_FILES" ]; then
+    echo "\033[42mNO CHANGES\033[0m\n"
+    cd "$WHEREAMI"
+    export PATH="$OLDPATH"
+    exit 0
 fi
 
 PASS=true
@@ -30,38 +38,36 @@ PASS=true
 echo "\nValidating Javascript:\n"
 
 # Check for eslint
-if [ ! -x "$ESLINT" ]; then
-  echo "\033[41mPlease install ESlint and dependencies\033[0m (npm i eslint-plugin-promise eslint-plugin-standard eslint-plugin-react)"
-  echo "\033[41mPlease install ESlint Standard config\033[0m (npm i eslint-config-standard)"
-  echo "\033[41mPlease install ESlint Semi-standard config\033[0m (npm i eslint-config-semistandard)"
-  exit 1
+if [ ! -f "$ESLINT" ]; then
+    echo "\033[41mPlease install ESlint and dependencies\033[0m (npm i eslint-plugin-promise eslint-plugin-standard eslint-plugin-react)"
+    echo "\033[41mPlease install ESlint Standard config\033[0m (npm i eslint-config-standard)"
+    echo "\033[41mPlease install ESlint Semi-standard config\033[0m (npm i eslint-config-semistandard)"
+    exit 1
 fi
 
-for FILE in $STAGED_FILES
-do
-    if [ -f "$FILE" ]; then
-        $ESLINT "$FILE"  --quiet -c "$HYBRIXD/common/hooks/eslintrc.js"
+while read -r FILE; do
+    "$ESLINT" "$FILE"  --quiet -c "$HYBRIXD/common/hooks/eslintrc.js"
 
-        if [ "$?" = "0" ]; then
-            echo "\033[32mESLint Passed: $FILE\033[0m"
-        else
-            PASS=false
-        fi
+    if [ "$?" -eq 0 ]; then
+        echo "\033[32mESLint Passed: $FILE\033[0m"
     else
-        echo "File was removed ESLint skipped: $FILE"
+        echo "\033[31mESLint Failed: $FILE\033[0m"
+        PASS=false
     fi
-done
+done < "$LINTLIST"
 
 echo "\nJavascript validation completed!\n"
 
-if [ "$PASS" = "false" ]; then
-  echo "\033[41mCOMMIT FAILED:\033[0m Your commit contains files that should pass ESLint but do not. Please fix the ESLint errors and try again.\n"
-  exit 1
+if ! $PASS; then
+    echo "\033[41mCOMMIT FAILED:\033[0m Your commit contains files that should pass ESLint but do not. Please fix the ESLint errors and try again.\n"
+    cd "$WHEREAMI"
+    export PATH="$OLDPATH"
+    exit 1
 else
-  echo "\033[42mCOMMIT SUCCEEDED\033[0m\n"
+    echo "\033[42mCOMMIT SUCCEEDED\033[0m\n"
 fi
 
-
-export PATH="$OLDPATH"
 cd "$WHEREAMI"
+export PATH="$OLDPATH"
+
 exit $?
