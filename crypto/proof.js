9c8040cea2535a5ddd7cf09006265a85c9fa7967
Rouke Pouw <roukepouw@gmail.com>
2019-04-02 12:32:18 +0000

    Update proof.js

--- crypto/proof.js
+++ crypto/proof.js
@@ -1,51 +1,83 @@
-var DJB2 = require('./hashDJB2');
+let DJB2 = require('./hashDJB2');
 
-function create (difficulty) {
-  var maxsize = (typeof difficulty === 'undefined' || isNaN(difficulty) ? 50000 : difficulty);
-  var pow = maxsize + (Math.random() * maxsize);
-  pow = String(Math.floor(pow + (Math.floor(Math.random() * 5) * (maxsize / 5))));
-  var prfx = '';
-  for (var i = 0; i < 5; i++) {
-    prfx += DJB2.hash('w1iV++8Paiz1W/JPPasdRTiHiPaZkF3L5fQBpl/EsMsAAAA=' + i + 'braNzUosoR/WGHdjuE3vgOzS3vAm0yeHdryKUKfqMzOZrnnxM2j0ZBtosjbG+BpuoONa0SSg0g1a' + String(Math.random() * (Math.random() * 1000000)) + 'H4sIALBvLloCA02OMQ4CMQwEe16xD0B8gIqKhk9YspOzyMUo8d2J35OYAgoX691Z+1YZtjkswRdB');
-  }
-  var sffx = DJB2.hash(String('rFGlQjLECtkQ7Lt2mCc+NCV4wbqX9VJehLZIsQGVN91vP+SUxmyOAAAA' + pow + prfx + 'y37v4RNP7wiH+kEdyYTTp/alYktUHe0gsATc9IuVhmeS4eX2/5zs4XTQ' + pow.split('').reverse().join('') + 'H4sIAHBsLloCAy2M0Q3CQAxD/5nCA1SMAQMwwVUXLpHSBF3SVmxPKvFn'));
-  return {hash: prfx + sffx, proof: DJB2.hash('PowerToThePeople_' + pow)};
+const TASK_GRANULARITY = 250;
+const DEFAULT_DIFFICULTY = 50000;
+const CONSTANT_A = 'w1iV++8Paiz1W/JPPasdRTiHiPaZkF3L5fQBpl/EsMsAAAA=';
+const CONSTANT_B = 'braNzUosoR/WGHdjuE3vgOzS3vAm0yeHdryKUKfqMzOZrnnxM2j0ZBtosjbG+BpuoONa0SSg0g1a';
+const CONSTANT_C = 'H4sIALBvLloCA02OMQ4CMQwEe16xD0B8gIqKhk9YspOzyMUo8d2J35OYAgoX691Z+1YZtjkswRdB';
+const CONSTANT_D = 'rFGlQjLECtkQ7Lt2mCc+NCV4wbqX9VJehLZIsQGVN91vP+SUxmyOAAAA';
+const CONSTANT_E = 'y37v4RNP7wiH+kEdyYTTp/alYktUHe0gsATc9IuVhmeS4eX2/5zs4XTQ';
+const CONSTANT_F = 'H4sIAHBsLloCAy2M0Q3CQAxD/5nCA1SMAQMwwVUXLpHSBF3SVmxPKvFn';
+const CONSTANT_G = 'PowerToThePeople_';
+
+function createHash (prfx, pow) {
+  return prfx + DJB2.hash(String(CONSTANT_D + String(pow) + prfx + CONSTANT_E + String(pow).split('').reverse().join('') + CONSTANT_F));
 }
 
-function solve (hash, func, fail, difficulty) {
-  var tasks = 250;
-  var maxsize = (typeof difficulty === 'undefined' || isNaN(difficulty) || Number(difficulty) <= 0 ? 100000 : difficulty * 2);
-  var pow = [];
-  var proofofwork = [];
-  var range = maxsize * tasks;
-  var prfx = hash.substr(0, 8 * 5);
-  for (var j = 0; j < tasks; j++) {
-    pow[j] = Math.floor(range / (tasks + 1)) * j;
-    proofofwork[j] = setInterval(function () {
-      var sffx = DJB2.hash(String('rFGlQjLECtkQ7Lt2mCc+NCV4wbqX9VJehLZIsQGVN91vP+SUxmyOAAAA' + pow[this.j] + prfx + 'y37v4RNP7wiH+kEdyYTTp/alYktUHe0gsATc9IuVhmeS4eX2/5zs4XTQ' + String(pow[this.j]).split('').reverse().join('') + 'H4sIAHBsLloCAy2M0Q3CQAxD/5nCA1SMAQMwwVUXLpHSBF3SVmxPKvFn'));
-      var powhash = prfx + sffx;
-      if (powhash === this.hash) {
-        for (k = 0; k < tasks; k++) {
-          clearInterval(proofofwork[k]);
-        }
-        if (typeof func === 'function') { func(DJB2.hash('PowerToThePeople_' + pow[this.j])); }
-      } else {
-        if (pow[this.j] < Math.floor(range / (this.j + 1) * j)) {
-          pow[this.j] += 1;
-        } else {
-          var complete = 0;
-          for (var k = 0; k < tasks; k++) {
-            if (pow[k] >= Math.floor(range / (this.j + 1) * j)) {
-              clearInterval(proofofwork[k]);
-              complete += 1;
-            }
-          }
-          if (complete >= (tasks * range)) {
-            if (typeof fail === 'function') { fail(); }
-          }
+function create (difficulty) {
+  difficulty = (typeof difficulty === 'undefined' || isNaN(difficulty) ? DEFAULT_DIFFICULTY : difficulty);
+
+  let pow = (1 + Math.random()) * difficulty; // Create a number between difficulty and 2 times difficulty
+
+  pow = Math.floor(pow + (Math.floor(Math.random() * 5) * (difficulty / 5))); // pow is now a number between difficulty and 3 times difficulty
+
+  let prfx = ''; // The prefix serves as a hint for the proof
+  for (let i = 0; i < 5; i++) {
+    prfx += DJB2.hash(CONSTANT_A + i + CONSTANT_B + String(Math.random() * (Math.random() * 1000000)) + CONSTANT_C);
+  }
+  return {hash: createHash(prfx, pow), proof: DJB2.hash(CONSTANT_G + String(pow))};
+}
+
+const executeTaskStep = (tasks, taskId, hint, difficulty, dataCallback, errorCallback, progressCallback) => () => {
+  // this process has been tasked to scan a 1/TASK_GRANULARITY part of all possible solutions
+  const task = tasks[taskId];
+  const step = task.step;
+  const maxSteps = Math.ceil(2 * difficulty / TASK_GRANULARITY); // the maximum nr of steps any process will handle
+
+  if (taskId === 0 && typeof progressCallback === 'function') {
+    progressCallback(step / (maxSteps + 1));
+  }
+
+  if (step < maxSteps) {
+    const prfx = hint.substr(0, 8 * 5);
+    const firstStep = difficulty + taskId * maxSteps;
+    const pow = firstStep + step; // Create a number between difficulty and 3 times difficulty
+    const hash = createHash(prfx, pow);
+    if (hint === hash) { // The hash is found, work is done
+      for (let k = 0; k < TASK_GRANULARITY; k++) { // clear all running processes
+        if (tasks[k].interval !== null) {
+          clearInterval(tasks[k].interval); // clear this process interval
         }
       }
-    }.bind({j: j, hash: hash}), 1);
+      if (typeof dataCallback === 'function') { dataCallback(DJB2.hash(CONSTANT_G + String(pow))); }
+    } else { // The hash is not yet found, next step
+      ++task.step;
+    }
+  } else { // nr of steps exceeded. This process failed, check if all steps have failed
+    let countFailed = 0;
+    for (let k = 0; k < TASK_GRANULARITY; k++) {
+      if (tasks[k].step >= maxSteps) {
+        countFailed += 1;
+      }
+    }
+    clearInterval(task.interval); // clear this process interval
+    task.interval = null;
+    if (countFailed === TASK_GRANULARITY) { // all processes have fails
+      if (typeof errorCallback === 'function') { errorCallback('Proof of Work failed.'); }
+    }
+  }
+};
+
+function solve (hint, difficulty, dataCallback, errorCallback, progressCallback) {
+  difficulty = (typeof difficulty === 'undefined' || isNaN(difficulty) ? DEFAULT_DIFFICULTY : difficulty);
+
+  const tasks = [];
+
+  for (let taskId = 0; taskId < TASK_GRANULARITY; taskId++) { // Subdivide the work into tasks
+    tasks[taskId] = {
+      step: 0,
+      interval: setInterval(executeTaskStep(tasks, taskId, hint, difficulty, dataCallback, errorCallback, progressCallback), 1)
+    };
   }
 }
 
