1764fab013440a3ed85c13d9262f4aee70230e0f
Rouke Pouw <roukepouw@gmail.com>
2020-01-09 15:00:36 +0000

    Add number and bool as recode option

--- basecode.js
+++ basecode.js
@@ -20,81 +20,80 @@ function generateAsciiTable () {
   for (let i = 0; i < 255; i++) { x = x + String.fromCharCode(i); }
   return x;
 }
+
 const asciiTable = generateAsciiTable();
 
-function recode (source, target, input) {
-  let BASE = function (val) {
-    let out;
-    switch (val) {
-      case 2: out = '01'; break;
-      case 8: out = '01234567'; break;
-      case 10: out = '0123456789'; break;
-      case 16: out = '0123456789abcdef'; break;
-      case 58: out = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'; break;
-      case 256: out = asciiTable; break;
-      case 'RFC4648': out = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567'; break;
-    }
-    return out;
-  };
-  if (typeof target === 'undefined') { target = source; source = BASE(256); }
-  switch (source) {
+function getEncoding (code) {
+  switch (code) {
     case 'ascii':
-      source = BASE(256);
-      break;
     case 'base256':
-      source = BASE(256);
-      break;
+      return asciiTable;
     case 'base58':
-      input = input.replace(/[^1-9A-HJ-NP-Za-km-z]/g, '');
-      source = BASE(58);
-      break;
+      return '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
     case 'hex':
-      source = BASE(16);
-      if (input.substring(0, 2) === '0x') { input = input.substring(2); }
-      input = input.toLowerCase().replace(/[^0-9a-f]/g, '');
-      break;
+      return '0123456789abcdef';
+    case 'number':
     case 'dec':
-      input = (typeof input === 'number') ? String(input) : input.replace(/[^0-9]/g, '');
-      source = BASE(10);
-      break;
+      return '0123456789';
     case 'oct':
-      input = input.replace(/[^0-7]/g, '');
-      source = BASE(8);
-      break;
+      return '01234567';
+    case 'bool':
     case 'bin':
-      input = input.replace(/[^0-1]/g, '');
-      source = BASE(2);
-      break;
+      return '01';
+    case 'bech32':
+    case 'RFC4648':
+      return 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
+    case 'string':
+    case 'utf-8':
     default:
-      if (source === 'string' || source === 'utf-8') { source = 'string'; }
-      if (source === 'bech32' || source === 'RFC4648') {
-        input = replaceBulk(input.toUpperCase(), ['0', '1', '8', '9'], ['O', 'I', 'B', 'G']).replace(/[^A-Z2-7]/g, '');
-        source = BASE('RFC4648');
-      }
-      break;
+      return 'string';
   }
-  switch (target) {
-    case 'ascii': target = BASE(256); break;
-    case 'base256': target = BASE(256); break;
-    case 'base58': target = BASE(58); break;
-    case 'hex': target = BASE(16); break;
-    case 'dec': target = BASE(10); break;
-    case 'oct': target = BASE(8); break;
-    case 'bin': target = BASE(2); break;
+}
+
+function cleanInput (code, input) {
+  switch (code) {
+    case 'base58':
+      return input.replace(/[^1-9A-HJ-NP-Za-km-z]/g, '');
+    case 'hex':
+      if (input.substring(0, 2) === '0x') { input = input.substring(2); }
+      return input.toLowerCase().replace(/[^0-9a-f]/g, '');
+    case 'number':
+    case 'dec':
+      return (typeof input === 'number') ? String(input) : input.replace(/[^0-9]/g, '');
+    case 'oct':
+      return input.replace(/[^0-7]/g, '');
+    case 'bool':
+      return input ? '1' : '0';
+    case 'bin':
+      return input.replace(/[^0-1]/g, '');
+    case 'bech32':
+    case 'RFC4648':
+      return replaceBulk(input.toUpperCase(), ['0', '1', '8', '9'], ['O', 'I', 'B', 'G']).replace(/[^A-Z2-7]/g, '');
     default:
-      if (target === 'string' || target === 'utf-8' || target === 'utf8') { target = 'string'; }
-      if (target === 'bech32' || target === 'RFC4648') { target = BASE('RFC4648'); }
-      if (!target) { target = 'string'; }
-      break;
+      return input;
   }
-  const buffer = Buffer.from(source === 'string' ? input : basex(source).decode(input));
-  let output;
-  if (target === 'string') {
-    output = Buffer.from(buffer.toString()).toString();
+}
+
+function recode (source, target, input) {
+  if (typeof target === 'undefined') { target = source; source = getEncoding('ascii'); }
+
+  const sourceEncoding = getEncoding(source);
+
+  input = cleanInput(source, input);
+
+  const buffer = Buffer.from(sourceEncoding === 'string' ? input : basex(sourceEncoding).decode(input));
+
+  const targetEncoding = getEncoding(target);
+
+  if (targetEncoding === 'string') {
+    return buffer.toString();
+  } else if (target === 'bool') {
+    return buffer.toString() !== '0';
+  } else if (target === 'number') {
+    return Number(Buffer.from(basex(targetEncoding).encode(buffer)));
   } else {
-    output = Buffer.from(basex(target).encode(buffer)).toString();
+    return Buffer.from(basex(targetEncoding).encode(buffer)).toString();
   }
-  return output;
 }
 
 exports.recode = recode;
